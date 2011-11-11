e3ff184e45c630b86fabea83bcbc43eeb13031a4
John Hinnegan <john.hinnegan@gmail.com>
2011-11-11 15:27:37 -0800

    plain vanilla rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datastores::Application.config.secret_token = 'd777cef9043cd7f38ecdea35d0edcdddf5b38531af24f1e539e6ef5df7c38d011dda527ffabcb84fefb07309650d5aa84cd631410ab895d6c958e90ca357b2f1'
